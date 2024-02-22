34c3133b11261e724883ae728fc860ee6b3b54ce
Malcolm Kahora <kahoram1@login001.localdomain>
2024-02-22 11:24:26 -0500

    Game loop done

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,12 +5,32 @@ class TicTacToe:
     def __init__(self):
         # Initialize the board, players, etc.
         self.board = [[None, None, None ], [None, None, None], [None, None, None], ]
+        self.positions = """
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+"""
         pass
     
     def play_game(self):
         # Main game loop
+        xTurn = False
+        while True:
+            user_input = int(input("Enter a board number to play"))
+            if user_input < 1 or user_input > 9:
+                print("Not a valid input")
+                continue
+            row = (user_input - 1) // 3
+            col = (user_input - 1) % 3
+
+            self.board[row][col] = 'X' if xTurn else 'O' 
+            xTurn = not xTurn
+            self.print_board()
         pass
     def print_board(self):
+        print(self.positions)
         for row in self.board:
             row_ = list(map(lambda a: ' ' if a == None else a,row))
             print("|{0}|{1}|{2}|".format(row_[0], row_[1], row_[2]))
@@ -30,4 +50,5 @@ if __name__ == "__main__":
     # Command line interface to run the game
     x = TicTacToe()
     x.print_board()
+    x.play_game()
     pass
