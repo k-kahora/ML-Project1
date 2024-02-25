ae6e1f30d1e820fe160342b3f4c5cc94e2365900
k-kahora <mango22mjk@gmail.com>
2024-02-25 18:59:28 +0000

    Reimplementing tic_tac_toe for AI

---
+++ tic_tac_toe_ai.py
@@ -0,0 +1,65 @@
+# tic_tac_toe.py
+
+def calculate_position(pos):
+    return (pos - 1) // 3, (pos - 1) % 3
+
+class TicTacToe:
+    def __init__(self):
+        # Initialize the board, players, etc.
+        self.xTurn = True
+        self.board = [[None, None, None ], [None, None, None], [None, None, None], ]
+        self.positions = """
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+"""
+    def alternate_player(self):
+        self.xTurn = not self.xTurn
+        return 'X' if self.xTurn else 'O'
+
+    def reset_board(self):
+        self.board = [[None, None, None ], [None, None, None], [None, None, None], ]
+    def make_move(self, row, col):
+        if (row < 0 or row > 2 or col < 0 or col > 2 or self.board[row][col] != None):
+            self.board[row][col] = self.alternate_player()
+        else:
+            assert "In valid move"
+        self.print_board()
+    
+    def calculate_draw(self):
+        return all(all(col is not None for col in row) for row in self.board)
+
+    def calculate_winner(self):
+        lines = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+        for row in lines:
+            a,b,c = [calculate_position(pos) for pos in row]
+            a_row, a_col = a
+            b_row, b_col = b
+            c_row, c_col = c
+            if (self.board[a_row][a_col] and self.board[a_row][a_col] == self.board[b_row][b_col] and self.board[a_row][a_col] == self.board[c_row][c_col]):
+                return self.board[a_row][a_col]
+        return None
+
+		# if (self.board[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+		# 	return squares[a];
+		# }
+
+    
+    def print_board(self):
+        print(self.positions)
+        for row in self.board:
+            row_ = list(map(lambda a: ' ' if a == None else a,row))
+            print("|{0}|{1}|{2}|".format(row_[0], row_[1], row_[2]))
+
+class AIPlayer:
+    def __init__(self, learning=False):
+        # Initialize AI player, with optional learning capability
+        pass
+    
+    def choose_move(self, board):
+        # Implement move choice logic here
+        pass
+# Other necessary classes/functions...
+
